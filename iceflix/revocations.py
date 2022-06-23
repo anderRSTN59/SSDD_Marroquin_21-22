7bc3f0c061e23b26a3905210fc213b3081c0c5ca
anderRSTN59 <58879565+anderRSTN59@users.noreply.github.com>
2022-06-23 20:14:32 +0200

    añadidas instancia del servicio de autenticación
    correccion Authenticator
    creacion revocations.py y  user_updates.py
    definidas revocations.py y  user_updates.py

---
+++ iceflix/revocations.py
@@ -0,0 +1,5 @@
+def revokeToken(userToken, srvId):
+    print("revoked token")
+
+def revokeUser(user, srvId):
+    print("revoked user")
---
+++ iceflix/user_updates.py
@@ -0,0 +1,5 @@
+def newUser(user, passwordHash, srvId):
+    new_user = user, passwordHash
+
+def newToken(user, userToken, srvId):
+    print ("")
